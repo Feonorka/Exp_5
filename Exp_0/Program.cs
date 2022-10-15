16dfe918beade24ce507fd38d486ed9d71f7e9b7
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-10-15 23:27:36 +0300

    Добавлена информация

--- Exp_0/Program.cs
+++ Exp_0/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Method(int maximum)
+{
+    int minimum;
+    minimum = -maximum;
+    while (minimum <= maximum)
+    {
+        Console.WriteLine(minimum + " ");
+        minimum++;
+    }
+}
+
+int[] CreateArray(int N)
+{
+    int[] arrayA = new int [N*2 + 1];
+    for (int i = -N; i < N; i++)
+    {
+        arrayA[i+ N] = i;
+    }
+    return arrayA;
+}
+// Лучший вариант
+int Ar(int N)
+{
+    int x = -N;
+    while (x <= N)
+    {
+        Console.WriteLine(x);
+        x++;    
+    }
+    return x;
+}
+
+void Numbers(int n)
+{
+    int lenght = n + n;
+    for (int i = 0; i < lenght +1; i++)
+    {
+        Console.WriteLine(-n + i);
+    }
+}
+// Худший
+string NumShow(int N)
+{
+    string NumsShow = "";
+for (int i = -N; i < N; i++)
+{
+    NumsShow = NumsShow + i + " ";
+}
+    return NumsShow;
+}
+
+int af = -5;
+int nf = 5;
+Console.WriteLine($"{af}..{nf}");
--- Exp_1/Program.cs
+++ Exp_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+bool IsEven(int value)
+{
+    return value % 2 == 0;
+}
+//* bool IsEven(int value) => value % 2 == 0;
+
+//** var IsEven = (int v) => v % 2 == 0;
+
+var a = 12;
+// Console.WriteLine(a.GetType());
+
+var data = new int[]{1,2,3,4}
+    .Where(e => e > 0)
+    .Select(e=> new{q= e, w = e + 1});
+
+Console.WriteLine(data.GetType().Name);
